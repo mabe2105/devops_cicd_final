3caca2cf2246980b33c1623ce31bf4ba238dff2b
Max <Max.Bechtel@yh.nackademin.se>
2022-11-29 22:03:48 +0100

    FIXED DB (in gh actions)

--- src/tests/e2e/test_selenium.py
+++ src/tests/e2e/test_selenium.py
@@ -51,9 +51,4 @@ def test_website():
     assert driver.current_url == f"{url}/auth/register"
 
 
-    driver.get(f"{url}/nothing")
-
-    assert driver.current_url == f"{url}/auth/register"
-
-
     driver.quit()
